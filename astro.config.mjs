a9e2e45dce2e19514c5bfe1f93d9988dbf9c8937
Arenaweb <67570426+arenawebrd@users.noreply.github.com>
2025-09-25 08:42:27 +0200

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   site: 'https://www.directoriord.com', // Replace with your actual domain
   integrations: [tailwind(), sitemap()],
 });
